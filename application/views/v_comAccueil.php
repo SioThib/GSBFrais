c8cb832030066a956cad899f7ed405acde2e72bb
therve <therve@B4-W7-40.sio.chaptalqper>
2019-03-28 14:46:04 +0100

    Mise en place d'un template pour les comptables

--- application/views/v_comAccueil.php
+++ application/views/v_comAccueil.php
@@ -1,10 +1,10 @@
 <div id="contenu">
-<!--  Test -->
+
 	<h2>Gestions des frais pour les comptables</h2>
-	<p>Bienvenue dans votre application de gestion des frais de déplacements. </p>
-	<p>
-		Afin de rembourser les frais de déplacement des visiteurs, vous avez accès aux fiches de frais mensuelles des visiteurs.
-		</p><ul>
+	<p> Bienvenue dans votre application de gestion des frais de déplacements. </p>
+	<p> Afin de rembourser les frais de déplacement des visiteurs, vous avez accès aux fiches de frais mensuelles des visiteurs.</p>
+	
+		<ul>
 			<li>Une fiche de frais court du 1er au dernier jour du mois;</li>
 			<li>Les fiches de frais sont créées automatiquement par l'application au fil de votre utilisation;</li>
 		</ul>
@@ -13,7 +13,8 @@
 	<p>
 		Au moyen du bandeau gauche, vous avez accès aux fonctionalités 
 		du profil comptable : 
-l	</p><ul>
+l	</p>
+       <ul>
 			<li>Gérer les fiches de frais des visiteurs(lire et valider)</li>
 			<li>Se déconnecter</li>
 		</ul>
